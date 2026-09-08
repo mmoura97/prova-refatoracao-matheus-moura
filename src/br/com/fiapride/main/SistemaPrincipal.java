7f5be44b6d8068200d876d4fe5453c27bea17a4e
mmoura97 <matheus.mourad30@gmail.com>
2026-09-07 23:10:59 -0300

    feat: finaliza refatoracao, encapsulamento e validacoes de seguranca

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -5,11 +5,12 @@ import br.com.fiapride.model.Veiculo;
 public class SistemaPrincipal {
     public static void main(String[] args) {
         Veiculo v1 = new Veiculo();
-        v1.individuo = "Carlos";
-        v1.pl = "ABC-1234";
-        v1.gas = -10; // X PERIGO: Valores negativos permitidos!
-        v1.adicionar(50);
-        v1.gasta(100); // X PERIGO: Consome mais que o disponivel!
-        System.out.println("Dono: " + v1.individuo + " | Placa: " + v1.pl + " | Gasolina: " + v1.gas);
+        
+        v1.setNivelCombustivel(-10); 
+        v1.abastecer(50);
+        v1.consumirCombustivel(100);
+        
+        System.out.println("Dono do veículo: " + v1.getMotorista());
+        
     }
 }
--- src/br/com/fiapride/model/Veiculo.java
+++ src/br/com/fiapride/model/Veiculo.java
@@ -1,15 +1,41 @@
 package br.com.fiapride.model;
 
 public class Veiculo {
-    public String individuo;
-    public String pl;
-    public int gas;
+    private String motorista;
+    private String placa;
+    private double nivelCombustivel;
 
-    public void adicionar(int v) {
-        gas = gas + v;
+    public void abastecer(double litros) {
+    	if (litros > 0) {
+    		nivelCombustivel = nivelCombustivel + litros;
+    	}else {
+    		System.out.println("Erro: Não é possível abastecer valores negativos.");
+    	}
+    	
     }
 
-    public void gasta(double v) {
-        gas = gas - v;
+    public void consumirCombustivel(double litros) {
+        if (litros > 0) {
+            if (nivelCombustivel >= litros) {
+                nivelCombustivel = nivelCombustivel - litros;
+            } else {
+                System.out.println("Erro: Combustível insuficiente para este consumo.");
+            }
+        } else {
+            System.out.println("Erro: O valor de consumo deve ser maior que zero.");
+        }
+    }
+    
+    
+    public String getMotorista() {
+    	return motorista;
+    }
+    
+    public void setNivelCombustivel(double litros) {
+        if (litros >= 0) {
+            nivelCombustivel = litros;
+        } else {
+            System.out.println("Erro: O nível inicial não pode ser menor que zero.");
+        }
     }
 }
